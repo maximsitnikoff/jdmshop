4d018c3ac40253feb9d4d582f4c493ee772a7dc2
maximsitnikoff <48902071+maximsitnikoff@users.noreply.github.com>
2019-03-25 09:04:03 +0300

    Add files via upload

---
+++ config/db_params.php
@@ -0,0 +1,23 @@
+<?php
+
+// Массив с параметрами подключения к базе данных
+return array(
+    'host' => 'localhost',
+    'dbname' => 'phpshop',
+    'user' => 'root',
+    'password' => '',
+);
+
+//return array(
+//    'host' => 'localhost',
+//    'dbname' => 'id7536684_phpshop',
+//    'user' => 'id7536684_phpshop',
+//    'password' => 'phpshop',
+//
+
+//return array(
+//    'host' => 'localhost',
+//    'dbname' => 'phpshop',
+//    'user' => 'root',
+//    'password' => '',
+//);
